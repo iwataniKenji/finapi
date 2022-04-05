a69ae00382dc69ec01e7d87e517390c381e4b51f
Kleverson Kenji <kleversonkenji@gmail.com>
2022-04-05 11:28:10 -0300

    nodemon, insomnia and http methods introduction

--- src/index.js
+++ src/index.js
@@ -2,9 +2,33 @@ const express = require("express");
 
 const app = express();
 
+app.use(express.json());
+
 // parâmetro do get -> rota, (req, res)
-app.get("/", (req, res) => {
-  return response.json({ message: "Hello World Ignite" });
+app.get("/courses", (req, res) => {
+  const query = req.query;
+  console.log(query);
+  return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+});
+
+app.post("/courses", (req, res) => {
+  const body = req.body;
+  console.log(body);
+  return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.put("/courses/:id", (req, res) => {
+  const { id } = req.params;
+  console.log(id);
+  return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
+});
+
+app.patch("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
+});
+
+app.delete("/courses/:id", (req, res) => {
+  return res.json(["Curso 6", "Curso 7", "Curso 4"]);
 });
 
 // listen -> inicia aplicação na porta 3333
