530a097ae204622bbeab06bcbb0c37cdb0f0c557
Kleverson Kenji <kleversonkenji@gmail.com>
2022-04-05 11:43:38 -0300

    fin api basic structure

--- src/index.js
+++ src/index.js
@@ -1,35 +1,28 @@
 const express = require("express");
+const { v4: uuidv4 } = require("uuid");
 
 const app = express();
 
 app.use(express.json());
 
-// parâmetro do get -> rota, (req, res)
-app.get("/courses", (req, res) => {
-  const query = req.query;
-  console.log(query);
-  return res.json(["Curso 1", "Curso 2", "Curso 3"]);
+const customers = [];
+
+app.post("/account", (req, res) => {
+  // informação vem da solicitação
+  const { cpf, name } = req.body;
+
+  // gera id aleatório
+  const id = uuidv4();
+
+  customers.push({
+    cpf,
+    name,
+    id,
+    statement: [],
+  });
+
+  return res.status(201).send();
 });
 
-app.post("/courses", (req, res) => {
-  const body = req.body;
-  console.log(body);
-  return res.json(["Curso 1", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.put("/courses/:id", (req, res) => {
-  const { id } = req.params;
-  console.log(id);
-  return res.json(["Curso 6", "Curso 2", "Curso 3", "Curso 4"]);
-});
-
-app.patch("/courses/:id", (req, res) => {
-  return res.json(["Curso 6", "Curso 7", "Curso 3", "Curso 4"]);
-});
-
-app.delete("/courses/:id", (req, res) => {
-  return res.json(["Curso 6", "Curso 7", "Curso 4"]);
-});
-
-// listen -> inicia aplicação na porta 3333
+// inicializa na porta 3333
 app.listen(3333);
